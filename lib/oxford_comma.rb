38ff9966cf9d7bc43449b0508ec14132326826b8
Brad Schrag <brad.schrag@gmail.com>
2018-11-04 17:28:45 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   final_value = array.pop
   statement = array.join(', ')
-  if array.count == 0
+  if array.count > 1
     statement = "#{statement}, and #{final_value}"
   elsif array.count == 1
     statement = "#{array[0]} and #{final_value}"
